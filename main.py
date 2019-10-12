6820639eec06d31094b3696d8c056740c2358a52
Marconi <marconi@marconicivitavecchia.it>
2019-10-12 09:51:35 +0200

    ad init functio

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-class Book :
-    
-    title = "no title"
-    autor = "no autor"
-    sbn = 0
+class Book : 
+    def __init__ (self,title,autor,sbn) :
+    self.title = title
+    self.autor = autor
+    self.sbn = sbn
 
-
-harry_potter = Book()
-
-
+harry_potter = Book( "Harry Potter e il calecie di fuoco","J.K. Rowling", 1)
 print(harry_potter.title)
